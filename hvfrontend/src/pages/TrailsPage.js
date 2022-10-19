88af17ce3c5855acd2567a8e66ba5161384ebc17
gdbecker <53883172+gdbecker@users.noreply.github.com>
2022-10-19 09:27:09 -0400

    Updates

--- hvfrontend/src/pages/TrailsPage.js
+++ hvfrontend/src/pages/TrailsPage.js
@@ -11,7 +11,19 @@ function TrailsPage({ user }) {
   const [trails, setTrails] = useState([]);
   const [reviews, setReviews] = useState([]);
   const [userFavorites, setUserFavorites] = useState([]);
+  const [parkFilter, setParkFilter] = useState([]);
+  const [stateFilter, setStateFilter] = useState([]);
+  const [difficultyFilter, setDifficultyFilter] = useState([]);
+  const [routeTypeFilter, setRouteTypeFilter] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [filterForm, setFilterForm] = useState({
+    park: '',
+    state: '',
+    difficulty: '',
+    routeType: ''
+  });
+  const onChange = e => setFilterForm({ ...filterForm, [e.target.name]: e.target.value });
 
   useEffect(() => {
     if (user) {
@@ -31,7 +43,14 @@ function TrailsPage({ user }) {
         await axios.get(`${process.env.REACT_APP_API_URL}/hvapp/trails/`, config)
          .then(function (response) {
            setTrails(response.data)
-
+           const uniqueParks = [...new Map(response.data.map(t => [t.park['id'], t.park])).values()];
+           setParkFilter(uniqueParks)
+           const uniqueStates = [...new Map(response.data.map(t => [t.park.state['id'], t.park.state])).values()];
+           setStateFilter(uniqueStates)
+           const uniqueDifficulty = [...new Map(response.data.map(t => [t.difficulty['id'], t.difficulty])).values()];
+           setDifficultyFilter(uniqueDifficulty)
+           const uniqueRouteTypes = [...new Map(response.data.map(t => [t.routetype['id'], t.routetype])).values()];
+           setRouteTypeFilter(uniqueRouteTypes)
          })
         .catch(function (error) {
            console.log(error);
@@ -80,6 +99,49 @@ function TrailsPage({ user }) {
           pageName={'trails'}
           img_url={Images.trail}
         />
+
+        <div className="container px-0 filter-form">
+          <div className="row g-2">
+            <div className="col-md-12">
+                <form>
+                  <div className="row g-2">
+                    <div className="col-md-3">
+                      <div className="form-group">
+                        <select className="form-control" onChange={e => onChange(e)} name="park">
+                          <option value="⬇️ filter by park ⬇️"> -- filter by park -- </option>
+                          {parkFilter.map((p, index) => <option key={index} value={p.id}>{p.name}</option>)}
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="form-group">
+                          <select className="form-control" onChange={e => onChange(e)} name="state">
+                            <option value="⬇️ filter by state ⬇️"> -- filter by state -- </option>
+                            {stateFilter.map((s, index) => <option key={index} value={s.id}>{s.full_name}</option>)}
+                          </select>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="form-group">
+                          <select className="form-control" onChange={e => onChange(e)} name="difficulty">
+                            <option value="⬇️ filter by difficulty ⬇️"> -- filter by difficulty -- </option>
+                            {difficultyFilter.map((d, index) => <option key={index} value={d.id}>{d.rank}</option>)}
+                          </select>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="form-group">
+                          <select className="form-control" onChange={e => onChange(e)} name="routeType">
+                            <option value="⬇️ filter by route type ⬇️"> -- filter by route type -- </option>
+                            {routeTypeFilter.map((r, index) => <option key={index} value={r.id}>{r.type}</option>)}
+                          </select>
+                        </div>
+                    </div>
+                  </div>
+                </form>
+            </div>
+          </div>
+        </div>
 
         <div className="container px-0">
           <div class="row g-2">
