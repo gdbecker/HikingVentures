88f0af616d92370a3f2386ea84496de790338c67
gdbecker <53883172+gdbecker@users.noreply.github.com>
2022-10-20 13:48:37 -0400

    Updates

--- hvfrontend/src/components/APIService.js
+++ hvfrontend/src/components/APIService.js
@@ -246,35 +246,4 @@ export default class APIService {
       body: body
     })
   }
-
-  static UpdateArticle(id, body) {
-    return fetch(`http://127.0.0.1:5000/update/${id}/`, {
-      'method':'PUT',
-      headers: {
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-    .then(resp => resp.json())
-  }
-
-  static InsertArticle(body) {
-    return fetch(`http://127.0.0.1:5000/add`, {
-      'method':'POST',
-      headers: {
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-    .then(resp => resp.json())
-  }
-
-  static DeleteArticle(id) {
-    return fetch(`http://127.0.0.1:5000/delete/${id}/`, {
-      'method':'DELETE',
-      headers: {
-        'Content-Type':'application/json'
-      }
-    })
-  }
 }
